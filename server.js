9466c5ce8869927c46aa028eb5a3255ae25b6866
davidsodaa <sodakolor@gmail.com>
2017-03-14 14:38:46 +0100

    Compression fixed*

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser");
 var compression = require('compression');
 
 app.use(compression())
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
